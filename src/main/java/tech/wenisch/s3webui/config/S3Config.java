511cf28dec52ff9fa3a75d29a73570b2ab20abb5
Jean-Fabian Wenisch <jean-fabian.wenisch@smail.auswaertiges-amt.de>
2026-03-27 16:36:45 +0100

    fix: add SKIP TLS Verify for s3

--- src/main/java/tech/wenisch/s3webui/config/S3Config.java
+++ src/main/java/tech/wenisch/s3webui/config/S3Config.java
@@ -1,5 +1,6 @@
 package tech.wenisch.s3webui.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,18 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.net.URI;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
+@Slf4j
 @Configuration
 public class S3Config {
 
@@ -28,8 +39,15 @@ public class S3Config {
     @Value("${s3.region:us-east-1}")
     private String region;
 
+    @Value("${s3.insecure-skip-tls-verify:false}")
+    private boolean s3InsecureSkipTlsVerify;
+
     @Bean
     public S3Client s3Client() {
+        if (s3InsecureSkipTlsVerify) {
+            enableInsecureTlsForS3();
+        }
+
         var credentials = AwsBasicCredentials.create(accessKey, secretKey);
         var builder = S3Client.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
@@ -49,6 +67,10 @@ public class S3Config {
 
     @Bean
     public S3Presigner s3Presigner() {
+        if (s3InsecureSkipTlsVerify) {
+            enableInsecureTlsForS3();
+        }
+
         var credentials = AwsBasicCredentials.create(accessKey, secretKey);
         var builder = S3Presigner.builder()
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
@@ -62,5 +84,40 @@ public class S3Config {
         }
 
         return builder.build();
+    }
+
+    private void enableInsecureTlsForS3() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            }};
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+
+            HostnameVerifier insecureHostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+            HttpsURLConnection.setDefaultHostnameVerifier(insecureHostnameVerifier);
+
+            log.warn("S3_INSECURE_SKIP_TLS_VERIFY is enabled. TLS certificate and hostname verification are disabled for HTTPS connections. Do not use this in production.");
+        } catch (GeneralSecurityException e) {
+            throw new IllegalStateException("Failed to enable insecure S3 TLS mode", e);
+        }
     }
 }
